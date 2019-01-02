d7d763c9f599731149b1ff9d8c68a01dc3abcb80
Jake Potrebic <jake.m.potrebic@gmail.com>
2019-01-02 15:59:00 -0800

    fixed logging

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,5 +1,5 @@
 module.exports = (bot, channel) => {
   const activeTrade = bot.activeTrades.filter(t => t.channel_id === channel.id)[0]
-  if (!activeTrade) return;
+  if (activeTrade.length < 1) return;
   activeTrade.del(true)
 }
