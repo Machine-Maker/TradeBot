eb5cc899ce16c3406e6e13e3d3a032f0af4b21e7
Jake Potrebic <jake.m.potrebic@gmail.com>
2019-01-04 15:05:13 -0800

    testing fixes for channelDelete

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,5 +1,7 @@
 module.exports = (bot, channel) => {
   const activeTrade = bot.activeTrades.find(t => t.channel_id === channel.id)
+  console.log(activeTrade)
   if (!activeTrade) return;
+  console.log("test")
   activeTrade.del(true)
 }
